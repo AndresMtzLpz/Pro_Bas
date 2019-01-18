2cdff735a80c2c2d5509d1f76fdf189f4489cfcd
Anes <andres.mtz,cu@gmail.com>
2019-01-18 00:56:01 -0600

    Todos los Layouts

---
+++ app/src/main/java/com/example/anes/pro_bas/Recetas.kt
@@ -0,0 +1,12 @@
+package com.example.anes.pro_bas
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Recetas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recetas)
+    }
+}
